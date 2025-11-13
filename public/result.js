f6b7858604b418eabc26b397f59a5a5ca909d068
singhone0515 <singhone0515@gmail.com>
2025-11-14 03:13:20 +0800

    update: 修改 server.js 與設定檔

--- public/result.js
+++ public/result.js
@@ -38,21 +38,21 @@ async function sendToServer(datatosearch) {
 
 }
 
-// function sortResult() {
-//   result.sort((a, b) => {
-//     const letterA = a.id[0];
-//     const letterB = b.id[0];
+function sortResult() {
+  result.sort((a, b) => {
+    const letterA = a.id[0];
+    const letterB = b.id[0];
 
-//     if (letterA !== letterB) {
-//       return letterA.localeCompare(letterB); // 先照字母排
-//     }
+    if (letterA !== letterB) {
+      return letterA.localeCompare(letterB); // 先照字母排
+    }
 
-//     // 字母相同 → 排數字
-//     const numA = Number(a.id.slice(1));
-//     const numB = Number(b.id.slice(1));
-//     return numA - numB;
-//   });
-// }
+    // 字母相同 → 排數字
+    const numA = Number(a.id.slice(1));
+    const numB = Number(b.id.slice(1));
+    return numA - numB;
+  });
+}
 
 function printResult(Result) {
   const tags = document.querySelector(".resultblock");
