fc0af11d347efa273e3bfeffc7495e4db81d6ce8
singhone0515 <singhone0515@gmail.com>
2025-11-15 17:21:46 +0800

    update: 修改 server.js 與設定檔

--- server.js
+++ server.js
@@ -181,6 +181,7 @@ app.post("/api/detail", (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log("✅ Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
